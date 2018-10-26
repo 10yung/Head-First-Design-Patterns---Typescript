6e8bd00a5333d5624d699ea66a87f1d5bf05ece5
TengYungLin <andynumber10@gmail.com>
2018-10-26 17:41:47 +0800

    delete commend

--- Strategy/gulpfile.js
+++ Strategy/gulpfile.js
@@ -2,11 +2,6 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
-// gulp.task("default", function () {
-//     return tsProject.src()
-//         .pipe(tsProject())
-//         .js.pipe(gulp.dest("./dist"));
-// });
 gulp.task('watch', () => {
   gulp.watch('**/*', {cwd: './src/'}, ['scripts']);
 });
